4557356a24c2039dab02ffbdc948d35a962c0fb8
Dan VerWeire <dverweire@gmail.com>
2018-08-21 13:06:43 -0400

    add async test

--- test.js
+++ test.js
@@ -94,3 +94,45 @@ test('base functionality via Promises', function (t) {
         t.notOk(err, 'no errors returned in add() callback');
     });
 });
+
+
+test('base functionality via async calls', async function (t) {
+    var db = new SymDb({
+        root : "/tmp/db"
+    });
+
+    var User = db.Model('user', {
+        name : String
+        , age : Number
+        , user_id : Number
+    });
+
+    var add = { 
+        name : 'Dan'
+        , age : 21
+        , user_id : 12345
+        , description : 'quartz'
+    };
+
+    var obj = await User.add(add);
+
+    t.ok(obj, 'user object returned in add() callback');
+    t.ok(obj._id, 'user object now contains an _id attribute in add() callback');
+
+    let records = await User.get(obj);
+
+    t.ok(records, 'records returned in get() callback');
+    t.equal(records.length, 1, 'one record returned in get() callback')
+    t.deepEqual(records[0], obj, 'the object returned in the get() callback matches the object we looked up')
+
+    obj.age = 39;
+
+    obj = await User.update(obj);
+
+    t.ok(obj, 'user object returned in update() callback');
+    t.equal(obj.age, 39, 'user object returned in update() callback is correct updated age');
+
+    await User.del(obj);
+
+    t.end();
+});
