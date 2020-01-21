cf8b2682b7aaa0f5faab37e9d9010c210795f0f4
Dan VerWeire <dverweire@gmail.com>
2020-01-21 00:25:46 -0500

    test: add sync test

--- test/sync.js
+++ test/sync.js
@@ -1,6 +1,63 @@
 var test = require('tape');
 var SymDb = require('..');
 
+test('sync: test comparisons in indexed fields', function (t) {
+    var db = new SymDb({
+        root : "/tmp/db"
+    });
+
+    var User = db.Model('user2', {
+        name : String
+        , age : Number
+        , user_id : Number
+    });
+
+    var add = { 
+        name : 'Dan'
+        , age : 21
+        , user_id : 12345
+        , description : 'quartz'
+    };
+
+    var obj = User.addSync(add);
+
+    t.ok(obj, 'user object returned in add() callback');
+    t.ok(obj._id, 'user object now contains an _id attribute in add() callback');
+
+    let records = User.getSync({
+        user_id : db.gt(20)
+    });
+
+    t.ok(records, 'records returned in get() callback');
+    t.equal(records.length, 1, 'one record returned in get() callback')
+    t.deepEqual(records[0], obj, 'the object returned in the get() callback matches the object we looked up')
+
+    records = User.getSync({
+        age : db.gt(25)
+    });
+
+    t.ok(records, 'records returned in get() callback');
+    t.equal(records.length, 0, 'no records returned in get() callback')
+
+    records = User.getSync({
+        age : db.gt(20)
+    });
+
+    t.ok(records, 'records returned in get() callback');
+    t.equal(records.length, 1, 'one record returned in get() callback')
+
+    records = User.getSync({
+        age : db.gte(21)
+    });
+
+    t.ok(records, 'records returned in get() callback');
+    t.equal(records.length, 1, 'no records returned in get() callback')
+
+    User.delSync(obj);
+
+    t.end();
+});
+
 test('sync: should add, get, update, delete synchronously non-indexed', function (t) {
     const db = new SymDb({
         root : "/tmp/db"
